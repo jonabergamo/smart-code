92324273e453fe6e74c1ad8d2a74648f7acfe36c
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-02-22 20:40:12 -0300

    1

---
+++ src/main.js
@@ -0,0 +1,16 @@
+const html = document.querySelector("html");
+const body = document.querySelector("body");
+var data = new Date();
+var horas = data.getHours();
+
+console.log(horas);
+
+window.addEventListener("load", function () {
+  if (horas < 18) {
+    html.classList.toggle("white-mode");
+    body.classList.toggle("white-mode");
+  }
+});
+
+//cole aqui o código JS da sua lógica 
+
