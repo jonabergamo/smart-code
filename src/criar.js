b3da0c9204ba8ac352d18cc316285c537b9feb6a
Jonathan Oliveira Bergamo <jonathanbergamo16@gmail.com>
2023-02-24 22:41:44 -0300

    1

--- src/criar.js
+++ src/criar.js
@@ -31,20 +31,63 @@ function criarHTML() {
   }, 500);
 }
 
-let aba = document.querySelector(".instrucoes");
+let abaInfo = document.querySelector(".instrucoes");
+let criarInfo = document.querySelector(".criarElemento");
 let icone = document.querySelector(".icone");
 var textos = document.querySelector(".textos");
+let iconeCriar = document.querySelector(".iconeCriar");
+var textosCriar = document.querySelector(".textosCriar");
+var expandir = document.querySelector(".expandir");
 
-aba.addEventListener("click", function () {
-  if (textos.style.display == 'flex') {
-    textos.style.display = 'none';
+abaInfo.addEventListener("dblclick", function () {
+  if (textos.style.display == "flex") {
+    textos.style.display = "none";
   } else {
     setTimeout(function () {
-      textos.style.display = 'flex';
+      textos.style.display = "flex";
     }, 150);
   }
-
   html.classList.toggle("expandir");
   body.classList.toggle("expandir");
 });
+criarInfo.addEventListener("dblclick", function () {
+  if (textosCriar.style.display == "flex") {
+    textosCriar.style.display = "none";
+  } else {
+    setTimeout(function () {
+      textosCriar.style.display = "flex";
+    }, 150);
+  }
 
+  html.classList.toggle("expandirCriar");
+  body.classList.toggle("expandirCriar");
+});
+var btnCriar = document.querySelector("#btnCriar");
+
+btnCriar.addEventListener("click", function () {
+
+  let selectElement = document.getElementById("selectElement");
+  let Elemento = selectElement.options[selectElement.selectedIndex].value;
+  let selectType = document.getElementById("selectAtribute");
+  let type = selectType.options[selectType.selectedIndex].value;
+  let nomeTipo = document.querySelector("#nomeTipo").value;
+  let conteudo = document.querySelector("#content").value;
+
+  if (document.querySelector("#html_enter").value == "") {
+    document.querySelector("#html_enter").value +=
+      `<${Elemento} ${type}="${nomeTipo}">` + `${conteudo}` + `</${Elemento}>`;
+  } else {
+    document.querySelector("#html_enter").value +=
+      `\n<${Elemento} ${type}="${nomeTipo}">` +
+      `${conteudo}` +
+      `</${Elemento}>`;
+  }
+});
+
+function resetHTML() {
+  document.querySelector("#html_enter").value = ""
+}
+
+function resetJS() {
+  document.querySelector("#js_enter").value = ""
+}
